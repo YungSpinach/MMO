f2efa3be39b2ddd340e8c9d96bae445bd7a03dc6
JPT <jparks02@dcs.bbk.ac.uk>
2025-02-18 14:12:47 +0000

    Update MMO_streamlit.py

--- MMO_streamlit.py
+++ MMO_streamlit.py
@@ -286,10 +286,10 @@ st.text("")
 fig, ax1 = plt.subplots(figsize=(10, 6))
 
 # Sort the output dataframe by Cover (%) in descending order
-#output_df = output_df.sort_values(by="Cover (%)", ascending=False)
+output_df = output_df.sort_values(by="Cover (%)", ascending=False)
 
 # Bar graph for Cover %
-bars = ax1.bar(output_df["Media Channel"], output_df["Cover (%)"], color='orangered', alpha=0.6)
+ax1.bar(output_df["Media Channel"], output_df["Cover (%)"], color='orangered', alpha=0.6)
 ax1.set_ylabel("Cover (%)", fontsize=14, fontweight='bold')
 ax1.tick_params(axis='y')
 
