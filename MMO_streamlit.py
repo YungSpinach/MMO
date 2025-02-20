05cd4864bf25fa8d0a04ac3e14ec76276231f3d7
JPT <jparks02@dcs.bbk.ac.uk>
2025-02-20 16:30:51 +0000

    Update MMO_streamlit.py

--- MMO_streamlit.py
+++ MMO_streamlit.py
@@ -10,7 +10,7 @@ st.title("Media Mix Optimiser :gear:")
 st.text("")
 st.write("### Input Parameters - required :pencil:")
 col1, col2 = st.columns(2)
-audience_name = col1.selectbox("Audience Name", ["ABC1 Adults", "Women 16-34", "ABC1 Women"])
+audience_name = col1.selectbox("Audience Name", ["ABC1 Adults", "Women 16-34", "ABC1 Women", "ABC1 Men"])
 total_budget = col2.number_input("Total Budget (£)", min_value=0, value=500000)
 marketing_objective = st.selectbox("Marketing Objective", ["Salience", "Unaided Awareness", "Aided Awareness", "Association", "Consideration", "Purchase Intent"])
 
